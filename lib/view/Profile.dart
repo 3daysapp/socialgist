cbd4cce8415c5ed17ae48663b6b849adfac106d4
Eduardo Folly <edufolly@gmail.com>
2020-05-10 10:28:06 -0300

    Changing profile screen to use CustomScrollView.

--- lib/view/Profile.dart
+++ lib/view/Profile.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:socialgist/i18n.dart';
 import 'package:socialgist/model/User.dart';
@@ -66,165 +67,180 @@ class _ProfileState extends State<Profile> {
         if (snapshot.hasData) {
           User me = snapshot.data;
 
-          List<Widget> profileInfoWidgets = [];
+          Map<String, Map<String, dynamic>> cards = {
+            'Following'.i18n: {
+              'qtd': me.following ?? 0,
+            },
+            'Followers'.i18n: {
+              'qtd': me.followers ?? 0,
+            },
+            'Repositories'.i18n: {
+              'qtd': (me.publicRepos ?? 0) + (me.totalPrivateRepos ?? 0),
+            },
+            'Gists'.i18n: {
+              'qtd': (me.publicGists ?? 0) + (me.privateGists ?? 0),
+            },
+          };
 
-          profileInfoWidgets.add(
-            Container(
-              margin: EdgeInsets.fromLTRB(15.0, 40.0, 15.0, 15.0),
-              child: Row(
-                children: [
-                  hasInfo(me.company)
-                      ? Expanded(
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: <Widget>[
-                              Icon(
-                                Icons.business,
-                                color: softWhite,
-                              ),
-                              SizedBox(height: 5.0),
-                              Text(
-                                me.company,
-                                style: Theme.of(context).textTheme.subtitle1,
-                              ),
-                            ],
-                          ),
-                        )
-                      : Spacer(),
-                  hasInfo(me.avatarUrl)
-                      ? Expanded(
-                          /// CircleAvatar as a parent with a larger size and
-                          /// background color to create a border effect.
-                          child: CircleAvatar(
-                            backgroundColor: Theme.of(context).accentColor,
-                            minRadius: 22.0,
-                            maxRadius: 52.0,
-                            child: CircleAvatar(
-                              backgroundColor: Colors.black54,
-                              backgroundImage: NetworkImage(me.avatarUrl),
-                              minRadius: 20.0,
-                              maxRadius: 50.0,
-                            ),
-                          ),
-                        )
-                      : Spacer(),
-                  hasInfo(me.location)
-                      ? Expanded(
+          return CustomScrollView(
+            slivers: [
+              SliverList(
+                delegate: SliverChildListDelegate(
+                  [
+                    /// Header Bar
+                    Container(
+                      margin: const EdgeInsets.fromLTRB(15.0, 40.0, 15.0, 15.0),
+                      child: Row(
+                        children: [
+                          /// Company
+                          hasInfo(me.company)
+                              ? Expanded(
+                                  child: Column(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: <Widget>[
+                                      Icon(
+                                        Icons.business,
+                                        color: softWhite,
+                                      ),
+                                      SizedBox(height: 5.0),
+                                      Text(
+                                        me.company,
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .subtitle1,
+                                      ),
+                                    ],
+                                  ),
+                                )
+                              : Spacer(),
+
+                          /// Avatar
+                          hasInfo(me.avatarUrl)
+                              ? Expanded(
+                                  /// CircleAvatar as a parent with a larger size and
+                                  /// background color to create a border effect.
+                                  child: CircleAvatar(
+                                    backgroundColor:
+                                        Theme.of(context).accentColor,
+                                    minRadius: 22.0,
+                                    maxRadius: 52.0,
+                                    child: CircleAvatar(
+                                      backgroundColor: Colors.black54,
+                                      backgroundImage:
+                                          NetworkImage(me.avatarUrl),
+                                      minRadius: 20.0,
+                                      maxRadius: 50.0,
+                                    ),
+                                  ),
+                                )
+                              : Spacer(),
+
+                          /// Location
+                          hasInfo(me.location)
+                              ? Expanded(
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: <Widget>[
+                                      Icon(
+                                        Icons.location_on,
+                                        color: softWhite,
+                                      ),
+                                      SizedBox(height: 5),
+                                      Text(
+                                        me.location,
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .subtitle1,
+                                        textAlign: TextAlign.center,
+                                      ),
+                                    ],
+                                  ),
+                                )
+                              : Spacer(),
+                        ],
+                      ),
+                    ),
+
+                    /// Name
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 2.0),
+                      child: Text(
+                        me.name ?? me.login,
+                        style: GoogleFonts.openSans(
+                          fontSize: 26,
+                          fontWeight: FontWeight.w700,
+                          color: softWhite,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+
+                    /// E-mail
+                    if (hasInfo(me.email))
+                      Padding(
+                        padding: const EdgeInsets.all(4.0),
+                        child: Text(
+                          me.email,
+                          style: Theme.of(context).textTheme.subtitle1,
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+
+                    /// Divider
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 10.0),
+                      child: Text(
+                        '__________',
+                        style: TextStyle(
+                          color: Theme.of(context).accentColor,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+
+                    /// Blog
+                    if (hasInfo(me.blog))
+                      Text(
+                        me.blog,
+                        style: Theme.of(context).textTheme.subtitle1,
+                        textAlign: TextAlign.center,
+                      ),
+                  ],
+                ),
+              ),
+              SliverPadding(
+                padding: const EdgeInsets.fromLTRB(15.0, 20.0, 15.0, 20.0),
+                sliver: SliverGrid.extent(
+                  maxCrossAxisExtent: 200.0,
+                  childAspectRatio: 1.2,
+                  mainAxisSpacing: 15.0,
+                  crossAxisSpacing: 15.0,
+                  children: cards.keys
+                      .map(
+                        (key) => Card(
                           child: Column(
                             mainAxisAlignment: MainAxisAlignment.center,
                             children: <Widget>[
-                              Icon(
-                                Icons.location_on,
-                                color: softWhite,
-                              ),
-                              SizedBox(height: 5),
                               Text(
-                                me.location,
+                                '${cards[key]['qtd']}',
+                                style: TextStyle(
+                                  fontSize: 50.0,
+                                  color: Theme.of(context).accentColor,
+                                ),
+                              ),
+                              Text(
+                                key,
                                 style: Theme.of(context).textTheme.subtitle1,
-                                textAlign: TextAlign.center,
                               ),
                             ],
                           ),
-                        )
-                      : Spacer(),
-                ],
-              ),
-            ),
-          );
-
-          profileInfoWidgets.add(
-            Padding(
-              padding: EdgeInsets.only(bottom: 2.0),
-              child: Text(
-                me.name ?? me.login,
-                style: GoogleFonts.openSans(
-                  fontSize: 26,
-                  fontWeight: FontWeight.w700,
-                  color: softWhite,
+                        ),
+                      )
+                      .toList(),
                 ),
               ),
-            ),
-          );
-
-          if (hasInfo(me.email)) {
-            profileInfoWidgets.add(Padding(
-              padding: const EdgeInsets.all(4.0),
-              child: Text(
-                me.email,
-                style: Theme.of(context).textTheme.subtitle1,
-              ),
-            ));
-          }
-
-          profileInfoWidgets.add(
-            Container(
-              width: 50,
-              margin: EdgeInsets.only(top: 10.0, bottom: 10.0),
-              child: Divider(
-                height: 1,
-                color: Theme.of(context).accentColor,
-                thickness: 1,
-              ),
-            ),
-          );
-
-          if (hasInfo(me.blog)) {
-            profileInfoWidgets.add(
-              Text(
-                me.blog,
-                style: Theme.of(context).textTheme.subtitle1,
-              ),
-            );
-          }
-
-          Map<String, int> cards = {
-            'Following'.i18n: me.following ?? 0,
-            'Followers'.i18n: me.followers ?? 0,
-            'Repositories'.i18n:
-                (me.publicRepos ?? 0) + (me.totalPrivateRepos ?? 0),
-            'Gists'.i18n: (me.publicGists ?? 0) + (me.privateGists ?? 0),
-          };
-
-          profileInfoWidgets.add(
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 25.0),
-              child: GridView.extent(
-                shrinkWrap: true,
-                maxCrossAxisExtent: 200.0,
-                childAspectRatio: 1.2,
-                mainAxisSpacing: 15.0,
-                crossAxisSpacing: 15.0,
-                children: cards.keys
-                    .map(
-                      (key) => Card(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            Text(
-                              '${cards[key]}',
-                              style: TextStyle(
-                                fontSize: 50.0,
-                                color: Theme.of(context).accentColor,
-                              ),
-                            ),
-                            Text(
-                              key,
-                              style: Theme.of(context).textTheme.subtitle1,
-                            ),
-                          ],
-                        ),
-                      ),
-                    )
-                    .toList(),
-              ),
-            ),
-          );
-
-          return SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: profileInfoWidgets,
-            ),
+            ],
           );
         }
 
